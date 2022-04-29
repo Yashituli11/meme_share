d001589567540cb4fb18b7af9f810172ee42be0e
Yashituli11 <83330758+Yashituli11@users.noreply.github.com>
2022-04-29 20:19:58 +0530

    Meme Share

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -9,7 +9,6 @@ import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
@@ -22,7 +21,7 @@ import com.bumptech.glide.request.target.Target
 class MainActivity : AppCompatActivity() {
     private lateinit var ivMemeShare:ImageView
     private lateinit var progressBar:ProgressBar
-    var currentImageUrl:String?=null
+   private var currentImageUrl:String?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
